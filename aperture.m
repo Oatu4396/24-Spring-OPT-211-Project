fb89185f4a7c00ee2d60310194620c1d17941149
Oatu4396 <oatu4396@gmail.com>
2024-04-24 10:17:54 -0400

    Update aperture.m

--- aperture.m
+++ aperture.m
@@ -3,7 +3,7 @@ figure
 imagesc(singleSlitAP()) % Plot image of the aperture field
 colormap gray % Set the color of the aperture field plot
 axis equal % Set the display scale of the axes
-axis([0 apl 0 apl]) % Set axes limits to size of aperture field
+axis([0 5000 0 5000]) % Set axes limits to size of aperture field
 
 function ap = squareAP
     apl=5000; % Size of the aperture field
