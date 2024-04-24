331b9269d23a35a85918ced0e6ab657b16beed65
Oatu4396 <oatu4396@gmail.com>
2024-04-24 12:38:22 -0400

    Tianqiao's Aperture

--- Apertures/TianqiaoAP.m
+++ Apertures/TianqiaoAP.m
@@ -1,10 +1,4 @@
 % Draw a '田' shaped aperture
-% Plot the aperture field
-figure
-imagesc(tianqiaoAP()) % Plot image of the aperture field
-colormap gray % Set the color of the aperture field plot
-axis equal % Set the display scale of the axes
-axis([0 apl 0 apl]) % Set axes limits to size of aperture field
 
 function ap = tianqiaoAP
     apl=5000; % Size of the aperture field
