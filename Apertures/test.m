cca38c61e11cc46ce1106e1a7b22e9bcffe512db
Oatu4396 <oatu4396@gmail.com>
2024-04-24 10:18:14 -0400

    Use this code to test your aperture

---
+++ Apertures/test.m
@@ -0,0 +1,6 @@
+% Plot the aperture field
+figure
+imagesc(doubleSlitAP()) % Plot image of the aperture field
+colormap gray % Set the color of the aperture field plot
+axis equal % Set the display scale of the axes
+axis([0 5000 0 5000]) % Set axes limits to size of aperture field
