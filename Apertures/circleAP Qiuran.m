8cfb77b5922da17f10d1240047142a5340ebb937
Qiuran17 <167327263+Qiuran17@users.noreply.github.com>
2024-04-28 22:51:22 -0400

    Add files via upload

---
+++ Apertures/circleAP Qiuran.m
@@ -0,0 +1,26 @@
+% Draw A Circular Aperture:
+% Define Aperture field
+apl = 5000; % Size of the aperture field
+ap = zeros(apl); % Define actual aperture plane
+
+% Define the center and the radius of the circle:
+circle_xcenter = apl / 2;
+circle_ycenter = apl / 2;
+r = 275; % The radius is 275 pixels.
+
+% Create a grid of (x,y) coordinates
+[X, Y] = meshgrid(1:apl, 1:apl);
+
+% Calculate the distance from the center to each point in the grid
+distances = sqrt((X - circle_xcenter).^2 + (Y - circle_ycenter).^2);
+
+% Set the points within the specified radius to 1
+ap(distances <= r) = 1;
+
+
+figure;
+imagesc(ap);
+colormap gray;
+axis equal;
+axis([0 apl 0 apl]);
+title('The Circular Aperture');
---
+++ Apertures/ringAP Qiuran.m
@@ -0,0 +1,26 @@
+% Draw A Circular Ring Aperture:
+% Define Aperture field
+apl = 5000; % Size of the aperture field
+ap = zeros(apl); % Define actual aperture plane
+
+% Define the center and the radius
+circle_xcenter = apl / 2;
+circle_ycenter = apl / 2;
+r1 = 275; % The bigger radius ==> 275 pixels.
+r2 = 175; % The smaller radius ==> 175 pixels.
+
+% Create a grid of (x,y) coordinates
+[X, Y] = meshgrid(1:apl, 1:apl);
+
+% Calculate the distance from the center to each point in the grid
+distances = sqrt((X - circle_xcenter).^2 + (Y - circle_ycenter).^2);
+
+ap(r2 <= distances & distances <= r1) = 1;
+
+
+figure;
+imagesc(ap);
+colormap gray;
+axis equal;
+axis([0 apl 0 apl]);
+title('The Circular Aperture');
