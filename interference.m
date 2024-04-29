13a5889339c3b0b419c93341b884a728fc2d98fd
Oatu4396 <oatu4396@gmail.com>
2024-04-29 15:59:54 -0400

    Add Circle Aperture

--- interference.m
+++ interference.m
@@ -24,6 +24,10 @@ function interference()
     buttons{1,3}.Text = 'Double Slit';
     buttons{1,3}.ButtonPushedFcn = @(btn,event) ...
         fourierInterference(doubleSlitAP());
+    % circle Aperture
+    buttons{1,3}.Text = 'Circle';
+    buttons{1,3}.ButtonPushedFcn = @(btn,event) ...
+        fourierInterference(circleAP());
     % Quit button
     buttons{2,5}.Text = 'Quit';
     buttons{2,5}.ButtonPushedFcn = @(btn,event) ...
@@ -112,6 +116,36 @@ function ap = doubleSlitAP
     return
 end
 
+function ap = circleAP
+    % Define Aperture field
+    apl = 5000; % Size of the aperture field
+    ap = zeros(apl); % Define actual aperture plane
+    
+    % Define the center and the radius of the circle:
+    circle_xcenter = apl / 2;
+    circle_ycenter = apl / 2;
+    r = 275; % The radius is 275 pixels.
+    
+    % Create a grid of (x,y) coordinates
+    [X, Y] = meshgrid(1:apl, 1:apl);
+    
+    % Calculate the distance from the center to each point in the grid
+    distances = sqrt((X - circle_xcenter).^2 + (Y - circle_ycenter).^2);
+    
+    % Set the points within the specified radius to 1
+    ap(distances <= r) = 1;
+    
+    
+    figure;
+    imagesc(ap);
+    colormap gray;
+    axis equal;
+    axis([0 apl 0 apl]);
+    title('The Circular Aperture');
+    
+    return
+end
+
 function ap = tianqiaoAP
     apl=5000; % Size of the aperture field
     ap=zeros(apl); % Define actual aperture plane
@@ -154,5 +188,35 @@ function ap = tianqiaoAP
             ap(j,i)=1;
         end
     end
+    return
+end
+
+function ap = qiuranRingAP
+    % Define Aperture field
+    apl = 5000; % Size of the aperture field
+    ap = zeros(apl); % Define actual aperture plane
+  
+    % Define the center and the radius
+    circle_xcenter = apl / 2;
+    circle_ycenter = apl / 2;
+    r1 = 275; % The bigger radius ==> 275 pixels.
+    r2 = 175; % The smaller radius ==> 175 pixels.
+  
+    % Create a grid of (x,y) coordinates
+    [X, Y] = meshgrid(1:apl, 1:apl);
+    
+    % Calculate the distance from the center to each point in the grid
+    distances = sqrt((X - circle_xcenter).^2 + (Y - circle_ycenter).^2);
+    
+    ap(r2 <= distances & distances <= r1) = 1;
+    
+    
+    figure;
+    imagesc(ap);
+    colormap gray;
+    axis equal;
+    axis([0 apl 0 apl]);
+    title('The Circular Aperture');
+  
     return
 end
