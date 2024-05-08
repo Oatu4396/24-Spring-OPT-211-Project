b74bbe20e8978a49d5bfa8d9c99f03a5ac8eb779
Qiuran17 <167327263+Qiuran17@users.noreply.github.com>
2024-05-08 10:56:54 -0400

    testAperture.m

--- Apertures/test.m
+++ Apertures/test.m
@@ -4,3 +4,7 @@ imagesc(YourApertureFunction()) % Plot image of the aperture field
 colormap gray % Set the color of the aperture field plot
 axis equal % Set the display scale of the axes
 axis([0 5000 0 5000]) % Set axes limits to size of aperture field
+title('(Which kind of) Aperture Field');
+xlabel('Position (x-axis) [Pixels]');
+ylabel('Position (y-axis) [Pixels]');
+colorbar
